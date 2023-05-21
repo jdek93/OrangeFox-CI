c77eee33b320126f4e164f413b06914ea69fe909
jdek93 <124425708+jdek93@users.noreply.github.com>
2023-05-21 16:48:42 -0700

    Update config.sh
    Replaced device to guamna Motorola

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 
 # Device
 export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export DT_LINK="https://gitlab.com/OrangeFox/device/guamna.git -b fox_9.0"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="guamna"
+export OEM="motorola"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
